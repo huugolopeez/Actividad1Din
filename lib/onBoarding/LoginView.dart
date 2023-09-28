396a05906fe6e216937d9fa84ec452f26eea419b
hugo <huugolopeez024@gmail.com>
2023-09-28 23:00:05 +0200

    Commit 4.0:  -Creacion del body en LoginView  -Creacion de los campos para el usuario y contraseña  -Creacion de los botones login y registrar pero sin funcionar

--- lib/onBoarding/LoginView.dart
+++ lib/onBoarding/LoginView.dart
@@ -10,11 +10,39 @@ class LoginView extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         shadowColor: Colors.deepPurpleAccent,
         title: Center(
-          child: Text('Login'),)
+          child: Text('Login'),
+        )
       ),
+      body: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Escribe tu usuario',
+                  ),
+                )
+            ),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Escribe tu contraseña',
+                  ),
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(onPressed: () { }, child: Text('Login'),),
+                TextButton(onPressed: () { }, child: Text('Registrar'),)
+              ],
+            ),
+          ]
+        ),
     );
 
     return scaffold;
   }
-
 }
