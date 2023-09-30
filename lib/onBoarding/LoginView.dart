2379b7d33d800bd109a461090814c50773f6ead4
hugo <huugolopeez024@gmail.com>
2023-09-30 12:37:35 +0200

    Commit 4.5:  -Retoque del body en loginView  -Implementacion de el boton register en loginView

--- lib/onBoarding/LoginView.dart
+++ lib/onBoarding/LoginView.dart
@@ -2,47 +2,50 @@ import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
 
+  late BuildContext _context;
+
+  onClickRegister() {
+    Navigator.of(_context).pushNamed('/registerview');
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    Scaffold scaffold = Scaffold(
+    _context = context;
+
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         shadowColor: Colors.deepPurpleAccent,
-        title: Center(
-          child: Text('Login'),
-        )
+        title: Center(child: Text('Login')),
       ),
-      body: Column(
-          children: [
-            Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Escribe tu usuario',
-                  ),
-                )
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
             ),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Escribe tu contraseña',
-                  ),
-                )
+          ),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(onPressed: () { }, child: Text('Login'),),
-                TextButton(onPressed: () { }, child: Text('Registrar'),)
-              ],
-            ),
-          ]
-        ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: () {  }, child: Text('Login')),
+              TextButton(onPressed: () { onClickRegister(); }, child: Text('Register')),
+            ],
+          )
+        ],
+      ),
     );
 
-    return scaffold;
   }
 }
