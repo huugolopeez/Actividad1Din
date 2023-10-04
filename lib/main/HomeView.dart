d7852d23f205fea9b2df8a610eaccacbe80033d0
hugo <huugolopeez024@gmail.com>
2023-10-04 19:40:17 +0200

    Commit 10.0:  -Implementacion del body en HomeView

--- lib/main/HomeView.dart
+++ lib/main/HomeView.dart
@@ -11,6 +11,18 @@ class HomeView extends StatelessWidget {
         shadowColor: Colors.deepPurpleAccent,
         title: Center(child: Text('Home'))
       ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Padding(padding: EdgeInsets.only(bottom: 16),
+                child: Text('Bienvenido al home de Actividad1', textScaleFactor: 2),
+              )
+          ),
+          Center(
+            child: Image.asset('resources/gatoElegante.jpg', width: 200),
+          )
+        ],
+      ),
     );
   }
 }
